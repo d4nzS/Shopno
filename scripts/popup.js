8d0f5fbbbf3d59e02b0ef69ff9f27117fbe147c9
d4nzS <dzianis.karym@gmail.com>
2022-04-11 13:00:03 +0300

    Remade popup.js with .forEach method

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,18 +4,18 @@ const popup = document.querySelector(".popup");
 const openPopupImages = document.querySelectorAll(".open-popup-image");
 const popupImage = document.querySelector(".popup__image");
 
-for (let i = 0; i < openPopupImages.length; i++) {
-    openPopupImages[i].addEventListener("click", () => {
-        const src = openPopupImages[i].getAttribute("src");
+openPopupImages.forEach(openPopupImage => {
+    openPopupImage.addEventListener("click", () => {
+        const src = openPopupImage.getAttribute("src");
 
         popup.classList.add("popup_active");
         popupImage.setAttribute("src", src);
     });
-}
+});
 
-for (let i = 0; i < openPopupImages.length; i++) {
+openPopupImages.forEach(openPopupImage => {
     popup.addEventListener("click", () => {
-        openPopupImages[i].classList.remove("open-popup-image_active");
+        openPopupImage.classList.remove("open-popup-image_active");
         popup.classList.remove("popup_active");
     });
-}
+});
